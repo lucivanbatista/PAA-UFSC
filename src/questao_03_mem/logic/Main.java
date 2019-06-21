1f1773c5f420c6ae2029a7bc4b65ec1a30c2108f
Lucivan Batista <lucivanbatista98@gmail.com>
2019-06-21 17:21:44 -0300

    Modificação testes da questao 03

--- src/questao_03_mem/logic/Main.java
+++ src/questao_03_mem/logic/Main.java
@@ -12,26 +12,35 @@ public class Main {
 		Caminhao a = new Caminhao(1, 10);
 		Caminhao b = new Caminhao(2, 15);
 		Caminhao c = new Caminhao(3, 8);
+		Caminhao d = new Caminhao(4, 100);
 		
 		List<Caminhao> T = new ArrayList<>();
 		T.add(a);
 		T.add(b);
 		T.add(c);
+		T.add(d);
 		
 		return T;
 	}
 	
 	public static List<Item> criarItens() {
-		Item x = new Item("tenis", 0, 20);
-		Item y = new Item("bola", 4, 0);
-		Item z = new Item("camisa", 5, 10);
-		Item k = new Item("geladeira", 100, 300);
+		Item t = new Item("Bicicleta", 9, 100);
+		Item u = new Item("Livro", 2, 0);
+		Item v = new Item("Churrasqueira", 200, 1000);
+		Item w = new Item("Geladeira", 100, 600);
+		Item x = new Item("Tenis", 2, 20);
+		Item y = new Item("Bola", 4, 3);
+		Item z = new Item("Camisa", 5, 10);
+		
 		
 		List<Item> G = new ArrayList<>();
+		G.add(t);
+		G.add(u);
+		G.add(v);
+		G.add(w);
 		G.add(x);
 		G.add(y);
 		G.add(z);
-		G.add(k);
 		
 		return G;
 	}
